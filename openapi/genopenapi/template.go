0f2596398c4f69a254d37df9567c68820c569ab4
梁晓东 <rover@xd.com>
2021-07-16 16:35:48 +0800

    change google.protobuf.Value=any

--- openapi/genopenapi/template.go
+++ openapi/genopenapi/template.go
@@ -82,7 +82,7 @@ var wktSchemas = map[string]schemaCore{
 	},
 	".google.protobuf.Value": {
 		//Type: "object",
-		Type: "",
+		Type: "any",
 	},
 	".google.protobuf.ListValue": {
 		Type: "array",
@@ -90,7 +90,7 @@ var wktSchemas = map[string]schemaCore{
 		//	Type: "object",
 		//}),
 		Items: (*openapiItemsObject)(&schemaCore{
-			Type: "",
+			Type: "any",
 		}),
 	},
 	".google.protobuf.NullValue": {
